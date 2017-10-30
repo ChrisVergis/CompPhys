005a4c19ae19ffa812a4f9a133c866071d63c19d
ChrisVergis <VerChri18@yahoo.com>
2017-10-30 15:55:17 +0100

    Update Lattice.cpp

--- Lattice.cpp
+++ Lattice.cpp
@@ -1,7 +1,7 @@
 #include "Lattice.h"
 
 Lattice::Lattice() : Lx(1.),Ly(1.) { }
-Lattice::Lattice(double x,double y) : Lx(x), Ly(y) { }
+Lattice::Lattice(double lx,double ly) : Lx(lx), Ly(ly) { }
 
 Lattice::setX(double x){  Lx = x; }
 Lattice::setY(double y){  Ly = y; }
